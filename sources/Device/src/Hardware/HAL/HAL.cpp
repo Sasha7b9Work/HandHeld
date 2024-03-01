381db264dcd0c492e142da267c55bcd9cfb12930
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-01 22:36:50 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL.cpp
+++ sources/Device/src/Hardware/HAL/HAL.cpp
@@ -1,10 +1,19 @@
 // 2024/03/01 22:24:09 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Hardware/HAL/HAL.h"
+#include "Hardware/HAL/systick.h"
+#include <gd32e23x.h>
 
 
 void HAL::Init()
 {
+    systick_config();
+
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOB);
+    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_GPIOF);
+
     i2cCOMMON.Init();
     i2cTFT.Init();
 }
