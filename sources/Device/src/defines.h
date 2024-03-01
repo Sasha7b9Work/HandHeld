1930c55a91ac2dd4316253b62cdc5a845d7d2ea0
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-01 21:33:01 +0300

    *

---
+++ sources/Device/src/defines.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
+    #pragma clang diagnostic ignored "-Wold-style-cast"
+    #pragma clang diagnostic ignored "-Wmissing-noreturn"
+#else
+#endif
