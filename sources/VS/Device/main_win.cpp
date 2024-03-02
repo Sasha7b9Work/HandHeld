1a35bcac8f4518201bb3e02ba7bbdc6490cdb922
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-02 14:00:16 +0300

    *

--- sources/VS/Device/main_win.cpp
+++ sources/VS/Device/main_win.cpp
@@ -1,8 +1,8 @@
 // 2022/04/29 10:12:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #define WIN32_LEAN_AND_MEAN
 #include "defines.h"
-#include "Application.h"
-#include "Frame.h"
+#include "GUI/Application.h"
+#include "GUI/Frame.h"
 #include "Device.h"
 #include "Hardware/Timer.h"
 
