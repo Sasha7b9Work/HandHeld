afb88b2b268d1a4a6d6c5679cb16bb8dfbc01bb6
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-04 19:12:31 +0300

    refact

--- sources/Device/src/Modules/PCF8563/PCF8563.cpp
+++ sources/Device/src/Modules/PCF8563/PCF8563.cpp
@@ -13,6 +13,48 @@
 #include "Modules/PCF8563/PCF8563.h"
 #include "Hardware/HAL/HAL.h"
 
+
+#define PCF8563_REG_CONTROL_STATUS1     ((uint8)0x00)
+#define PCF8563_REG_CONTROL_STATUS2     ((uint8)0x01)
+#define PCF8563_REG_TIME                ((uint8)0x02)
+#define PCF8563_REG_SECONDS             ((uint8)0x02)
+#define PCF8563_REG_MINUTES             ((uint8)0x03)
+#define PCF8563_REG_HOURS               ((uint8)0x04)
+#define PCF8563_REG_DATE                ((uint8)0x05)
+#define PCF8563_REG_WEEKDAY             ((uint8)0x06)
+#define PCF8563_REG_MONTH               ((uint8)0x07)
+#define PCF8563_REG_YEAR                ((uint8)0x08)
+#define PCF8563_REG_ALARM_MINUTE        ((uint8)0x09)
+#define PCF8563_REG_ALARM_HOUR          ((uint8)0x0A)
+#define PCF8563_REG_ALARM_DAY           ((uint8)0x0B)
+#define PCF8563_REG_ALARM_WEEKDAY       ((uint8)0x0C)
+#define PCF8563_REG_CLKOUT              ((uint8)0x0D)
+#define PCF8563_REG_TIMER_CONTROL       ((uint8)0x0E)
+#define PCF8563_REG_TIMER               ((uint8)0x0F)
+
+ //
+ //	Controll register 1 0x00
+ //
+#define PCF8563_CONTROL1_TEST1		7
+#define PCF8563_CONTROL1_STOP		5
+#define PCF8563_CONTROL1_TESTC		3
+
+//
+//	Controll register 2 0x01
+//
+#define PCF8563_CONTROL2_TI_TP		4
+#define PCF8563_CONTROL2_AF			3
+#define PCF8563_CONTROL2_TF			2
+#define PCF8563_CONTROL2_AIE		1
+#define PCF8563_CONTROL2_TIE		0
+
+//
+//	CLKOUT control register 0x0D
+//
+#define PCF8563_CLKOUT_CONTROL_FE				7
+#define PCF8563_CLKOUT_CONTROL_FD1				1
+#define PCF8563_CLKOUT_CONTROL_FD0				0
+
 static uint8 Pcf8563Buffer[7];
 
 
