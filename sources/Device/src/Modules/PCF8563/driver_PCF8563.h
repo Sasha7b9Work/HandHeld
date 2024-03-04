6f1124e5aff160a6d0253deaef90f58f876427c1
Sasha7b9Work <Phuntik-it@tut.by>
2024-03-04 10:39:23 +0300

    Добавлен файл драйвера из интернета2

--- sources/Device/src/Modules/PCF8563/driver_PCF8563.h
+++ sources/Device/src/Modules/PCF8563/driver_PCF8563.h
@@ -1,40 +1,39 @@
 /*
  * PCF8563.h
  *
- *	The MIT License.
+ *  The MIT License.
  *  Created on: 5.09.2019
  *      Author: Mateusz Salamon
- *		Contact: mateusz@msalamon.pl
+ *      Contact: mateusz@msalamon.pl
  *
  *      Website: https://msalamon.pl/dalsze-zmagania-z-rtc-ds1307-i-pcf8563-na-stm32/
  *      GitHub: https://github.com/lamik/PCF8563_RTC_STM32_HAL
  */
 
 //
-//	Uncomment when you are using DMA reading
+// Uncomment when you are using DMA reading
 //
-#define PCF8563_USE_DMA
 
-#define PCF8563_ADDRESS              	(0x51<<1)
-#define PCF8563_I2C_TIMEOUT				100
+#define PCF8563_ADDRESS                 (0x51<<1)
+#define PCF8563_I2C_TIMEOUT             100
 
-#define PCF8563_REG_CONTROL_STATUS1     0x00
-#define PCF8563_REG_CONTROL_STATUS2     0x01
-#define PCF8563_REG_TIME         		0x02
-#define PCF8563_REG_SECONDS          	0x02
-#define PCF8563_REG_MINUTES         	0x03
-#define PCF8563_REG_HOURS          		0x04
-#define PCF8563_REG_DATE             	0x05
-#define PCF8563_REG_WEEKDAY             0x06
-#define PCF8563_REG_MONTH            	0x07
-#define PCF8563_REG_YEAR             	0x08
-#define PCF8563_REG_ALARM_MINUTE        0x09
-#define PCF8563_REG_ALARM_HOUR       	0x0A
-#define PCF8563_REG_ALARM_DAY   		0x0B
-#define PCF8563_REG_ALARM_WEEKDAY    	0x0C
-#define PCF8563_REG_CLKOUT    			0x0D
-#define PCF8563_REG_TIMER_CONTROL    	0x0E
-#define PCF8563_REG_TIMER     			0x0F
+#define PCF8563_REG_CONTROL_STATUS1     ((uint8)0x00)
+#define PCF8563_REG_CONTROL_STATUS2     ((uint8)0x01)
+#define PCF8563_REG_TIME                ((uint8)0x02)
+#define PCF8563_REG_SECONDS             ((uint8)0x02)
+#define PCF8563_REG_MINUTES             ((uint8)0x03)
+#define PCF8563_REG_HOURS               ((uint8)0x04)
+#define PCF8563_REG_DATE                ((uint8)0x05)
+#define PCF8563_REG_WEEKDAY             ((uint8)0x06)
+#define PCF8563_REG_MONTH               ((uint8)0x07)
+#define PCF8563_REG_YEAR                ((uint8)0x08)
+#define PCF8563_REG_ALARM_MINUTE        ((uint8)0x09)
+#define PCF8563_REG_ALARM_HOUR          ((uint8)0x0A)
+#define PCF8563_REG_ALARM_DAY           ((uint8)0x0B)
+#define PCF8563_REG_ALARM_WEEKDAY       ((uint8)0x0C)
+#define PCF8563_REG_CLKOUT              ((uint8)0x0D)
+#define PCF8563_REG_TIMER_CONTROL       ((uint8)0x0E)
+#define PCF8563_REG_TIMER               ((uint8)0x0F)
 
 //
 //	Controll register 1 0x00
@@ -61,39 +60,37 @@
 
 typedef enum
 {
-	CLKOUT_FREQ_1HZ 		= 3,
-	CLKOUT_FREQ_32HZ 		= 2,
-	CLKOUT_FREQ_1024HZ 		= 1,
-	CLKOUT_FREQ_32768HZ 	= 0
-}CLKOUT_Freq;
+    CLKOUT_FREQ_32768HZ = 0,
+    CLKOUT_FREQ_1024HZ  = 1,
+    CLKOUT_FREQ_32HZ    = 2,
+    CLKOUT_FREQ_1HZ     = 3,
+	Count
+} CLKOUT_Freq;
 
 typedef struct
 {
-	uint16_t 	Year;
-	uint8_t  	Month;
-	uint8_t		Day;
-	uint8_t		Hour;
-	uint8_t		Minute;
-	uint8_t		Second;
-	uint8_t		DayOfWeek;
+	uint16 	Year;
+	uint8  	Month;
+	uint8		Day;
+	uint8		Hour;
+	uint8		Minute;
+	uint8		Second;
+	uint8		DayOfWeek;
 }RTCDateTime;
 
-void PCF8563_TEST1Enable(uint8_t Enable);
-void PCF8563_STOPEnable(uint8_t Enable);
-void PCF8563_TESTCEnable(uint8_t Enable);
-void PCF8563_InterruptEnable(uint8_t Enable);
-void PCF8563_AlarmFlagEnable(uint8_t Enable);
-void PCF8563_TimerFlagEnable(uint8_t Enable);
-void PCF8563_AlarmInterruptEnable(uint8_t Enable);
-void PCF8563_TimerInterruptEnable(uint8_t Enable);
+void PCF8563_TEST1Enable(uint8 Enable);
+void PCF8563_STOPEnable(uint8 Enable);
+void PCF8563_TESTCEnable(uint8 Enable);
+void PCF8563_InterruptEnable(uint8 Enable);
+void PCF8563_AlarmFlagEnable(uint8 Enable);
+void PCF8563_TimerFlagEnable(uint8 Enable);
+void PCF8563_AlarmInterruptEnable(uint8 Enable);
+void PCF8563_TimerInterruptEnable(uint8 Enable);
 
 void PCF8563_ClkoutFrequency(CLKOUT_Freq Frequency);
 
-#ifdef PCF8563_USE_DMA
-void PCF8563_ReceiveDateTimeDMA(void);	// Use in PCF8563 Interrupt handler
-void PCF8563_CalculateDateTime(RTCDateTime *DateTime);	// Use in DMA Complete Receive interrupt
-#else
 void PCF8563_GetDateTime(RTCDateTime *DateTime);	// Use in blocking/interrupt mode in PCF8563_INT EXTI handler
-#endif
 
-void PCF8563_Init(I2C_HandleTypeDef *hi2c);
+void PCF8563_SetDateTime(RTCDateTime *DateTime);
+
+void PCF8563_Init();
