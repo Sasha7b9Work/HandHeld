a6f59ffb695b2fbe9c2d115f155845273d74a9e7
Sasha7b9Work <Phuntik-it@tut.by>
2024-03-06 13:57:33 +0300

    *

--- sources/Device/src/Keyboard/Keyboard.cpp
+++ sources/Device/src/Keyboard/Keyboard.cpp
@@ -1,11 +1,47 @@
 // 2024/03/01 23:01:40 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Keyboard/Keyboard.h"
+#include <gd32e23x.h>
+
+
+namespace Keyboard
+{
+    struct Button
+    {
+        Button(uint _port, uint16 _pin) : port(_port), pin(_pin) { }
+
+        void Init()
+        {
+            gpio_mode_set(port, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, pin);
+        }
+
+        bool IsDown()
+        {
+            return (gpio_input_bit_get(port, pin) == RESET);
+        }
+        bool IsUp()
+        {
+            return !IsDown();
+        }
+
+    private:
+        uint port;
+        uint16 pin;
+    };
+
+    static Button btnMenu(GPIOB, GPIO_PIN_0);
+    static Button btnCancel(GPIOB, GPIO_PIN_2);
+    static Button btnUp(GPIOA, GPIO_PIN_7);
+    static Button btnDown(GPIOB, GPIO_PIN_1);
+}
 
 
 void Keyboard::Init()
 {
-
+    btnMenu.Init();
+    btnCancel.Init();
+    btnUp.Init();
+    btnDown.Init();
 }
 
 
