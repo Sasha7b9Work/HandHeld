2e276fba5d5d0e0fb95867207b0a120cc37af813
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-10 16:01:36 +0300

    *

--- sources/Device/src/Menu/Pages/PageCalls/PageSoundSensor.cpp
+++ sources/Device/src/Menu/Pages/PageCalls/PageSoundSensor.cpp
@@ -2,9 +2,63 @@
 #include "defines.h"
 #include "Menu/Pages/Pages.h"
 #include "Menu/MenuItemDef.h"
+#include "Settings/Settings.h"
 
 
-DEF_PAGE_0(pageSoundSensor, PageCalls::self, "Датчик звука");
+namespace PageCalls
+{
+    namespace PageSoundSensor
+    {
+        DEF_CHOICE_10(choiceMelody, self, "Мелодия", &gset.sources[Source::SoundSensor].melody,
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"
+        );
+
+        DEF_CHOICE_10(choiceVolume, self, "Громкость", &gset.sources[Source::SoundSensor].volume,
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"
+        );
+
+        DEF_CHOICE_2(choiceEnableLED, self, "Светодиод", &gset.sources[Source::SoundSensor].enabled_led,
+            "Откл",
+            "Вкл"
+        );
+
+        DEF_CHOICE_2(choiceEnableMelody, self, "Звук", &gset.sources[Source::SoundSensor].enabled_melody,
+            "Откл",
+            "Вкл"
+        );
+
+        DEF_CHOICE_2(choiceEnabledVibro, self, "Вибрация", &gset.sources[Source::SoundSensor].enabled_vibrato,
+            "Откл",
+            "Вкл"
+        );
 
 
-const Item *const PageCalls::PageSoundSensor::self = &pageSoundSensor;
+        DEF_PAGE_5(pageSoundSensor, PageCalls::self, "Датчик звука",
+            &choiceMelody,
+            &choiceVolume,
+            &choiceEnableLED,
+            &choiceEnableMelody,
+            &choiceEnabledVibro
+        );
+
+        const Item *const self = &pageSoundSensor;
+    }
+}
