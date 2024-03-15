416725344267e4929425a94965147719534534cc
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-16 00:33:34 +0300

    refact

--- sources/Device/src/Modules/CMT2210AW/CMT2210AW.cpp
+++ sources/Device/src/Modules/CMT2210AW/CMT2210AW.cpp
@@ -200,12 +200,20 @@ void CMT2210AW::Data::VerifyPreambule1()
         if (bitlevel < (11 - BARKERTRESHOLD))
             return;
 
-    //                           H6      H5      H4      H2    H1     H0     P0
-    static const int index[] = { 0,      0,      0,      1,    1,     1,     2 };
-    static const int shift[] = { 26,     15,     4,      46,   35,    24,    0 };
-    static const uint bit[] = { 0x4000, 0x2000, 0x1000, 0x400, 0x200, 0x100, 0x1 };
+    //check bit PAYLOAD 5
+    bitlevel = (uint)(BITSSETTABLEFF[(uint16_t)((xors[2] >> 55) & 0x07FF)] + BITSSETTABLEFF[(uint16_t)(xors[1] & 0x03)]);
+    if (bitlevel < BARKERTRESHOLD)
+        packet |= 0x0020;
+    else
+        if (bitlevel < (11 - BARKERTRESHOLD))
+            return;
 
-    for (int i = 0; i < 7; i++)
+    //                           H6      H5      H4      H2    H1     H0     P0   P1   P2   P3   P4    P6    P7
+    static const int index[] = { 0,      0,      0,      1,    1,     1,     2,   2,   2,   2,   2,    1,    1 };
+    static const int shift[] = { 26,     15,     4,      46,   35,    24,    0,   11,  22,  33,  44,   2,    13 };
+    static const uint bit[] = { 0x4000, 0x2000, 0x1000, 0x400, 0x200, 0x100, 0x1, 0x2, 0x4, 0x8, 0x10, 0x40, 0x80 };
+
+    for (int i = 0; i < 13; i++)
     {
         uint level = BITSSETTABLEFF[(xors[index[i]] >> shift[i]) & 0x7FF];
         if (level < BARKERTRESHOLD)
@@ -217,62 +225,6 @@ void CMT2210AW::Data::VerifyPreambule1()
             return;
         }
     }
-
-    //check bit PAYLOAD 1
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[2] >> 11) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0002;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 2
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[2] >> 22) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0004;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 3
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[2] >> 33) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0008;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 4
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[2] >> 44) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0010;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 5
-    bitlevel = (uint)(BITSSETTABLEFF[(uint16_t)((xors[2] >> 55) & 0x07FF)] + BITSSETTABLEFF[(uint16_t)(xors[1] & 0x03)]);
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0020;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 6
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[1] >> 2) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0040;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
-
-    //check bit PAYLOAD 7
-    bitlevel = BITSSETTABLEFF[(uint16_t)((xors[1] >> 13) & 0x07FF)];
-    if (bitlevel < BARKERTRESHOLD)
-        packet |= 0x0080;
-    else
-        if (bitlevel < (11 - BARKERTRESHOLD))
-            return;
 
     if (packet == 0x7E8D)
     {
