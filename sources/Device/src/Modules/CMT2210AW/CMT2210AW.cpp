75a7b25ea1c056ecd2a14492b031ca669347b447
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-16 01:11:20 +0300

    *

--- sources/Device/src/Modules/CMT2210AW/CMT2210AW.cpp
+++ sources/Device/src/Modules/CMT2210AW/CMT2210AW.cpp
@@ -168,7 +168,7 @@ void CMT2210AW::Data::AppendBit(bool bit)
 
 uint CMT2210AW::Data::GetBits(uint64 bits)
 {
-    static const unsigned byte_count[256] =
+    static const uint byte_count[256] =
     {
         0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
         1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
