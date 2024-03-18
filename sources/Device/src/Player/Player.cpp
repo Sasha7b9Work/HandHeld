d35554e3ded3cdba43e36a6989a5fef0fc4b65ff
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-18 20:25:31 +0300

    *

--- sources/Device/src/Player/Player.cpp
+++ sources/Device/src/Player/Player.cpp
@@ -9,6 +9,12 @@
 namespace Player
 {
     static uint8 NextSample(float frequency);
+
+    static float k = 0.5f;
+
+    static float frequency = 5274.0f;
+
+    static uint prev_time = 0;
 }
 
 
@@ -20,7 +26,23 @@ void Player::Init()
 
 void Player::CallbackOnTimer()
 {
-    Beeper::CallbackOnOutputSample(NextSample(1234.0f));
+    Beeper::CallbackOnOutputSample(NextSample(frequency));
+
+    if (TIME_MS > prev_time + 1000)
+    {
+        prev_time = TIME_MS;
+
+        frequency *= k;
+
+        if (frequency > 6000.0f)
+        {
+            k = 0.5f;
+        }
+        else if (frequency < 60.0f)
+        {
+            k = 2.0f;
+        }
+    }
 }
 
 
