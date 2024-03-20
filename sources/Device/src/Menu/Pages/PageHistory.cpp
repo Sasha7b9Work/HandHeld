056cc11383686864c367ea0425c951ba73a4bf43
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-03-20 21:27:12 +0300

    *

--- sources/Device/src/Menu/Pages/PageHistory.cpp
+++ sources/Device/src/Menu/Pages/PageHistory.cpp
@@ -12,7 +12,12 @@ namespace PageHistory
 
     static void DrawRecord(int x, int y, int num_record)
     {
-        Text<>("%d/%d", num_record, Storage::GetCountRecords()).Write(x, y, Color::WHITE);
+        if (num_record >= Storage::GetCountRecords())
+        {
+            return;
+        }
+
+        Text<>("%d/%d", num_record + 1, Storage::GetCountRecords()).Write(x, y, Color::WHITE);
     }
 
     static void FuncDraw_History()
@@ -20,17 +25,34 @@ namespace PageHistory
         HLine(160).Draw(0, 26, Color::WHITE);
         HLine(160).Draw(0, 53);
 
-        DrawRecord(0, 0, 0);
+        DrawRecord(0, 0, top_record);
 
-        DrawRecord(0, 28, 1);
+        DrawRecord(0, 28, (top_record + 1) % Storage::GetCountRecords());
 
-        DrawRecord(0, 55, 2);
+        DrawRecord(0, 55, (top_record + 2) % Storage::GetCountRecords());
     }
 
     static bool Func_ActionKey(const Action &action)
     {
         if (action.key == Key::Up || action.key == Key::Down)
         {
+            if (action.key == Key::Up)
+            {
+                top_record--;
+                if (top_record == -1)
+                {
+                    top_record = Storage::GetCountRecords() - 1;
+                }
+            }
+            else if (action.key == Key::Down)
+            {
+                top_record++;
+                if (top_record == Storage::GetCountRecords())
+                {
+                    top_record = 0;
+                }
+            }
+
             return true;
         }
 
