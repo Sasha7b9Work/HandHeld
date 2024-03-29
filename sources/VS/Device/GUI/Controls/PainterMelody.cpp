362ded0cf42ffd76ac6a4884757a278630980cb7
Sasha7b9Work <Phuntik-it@tut.by>
2024-03-29 12:11:10 +0300

    *

---
+++ sources/VS/Device/GUI/Controls/PainterMelody.cpp
@@ -0,0 +1,3 @@
+// 2024/03/29 12:07:13 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines_win.h"
+#include "GUI/Controls/PainterMelody.h"
---
+++ sources/VS/Device/GUI/Controls/PainterMelody.h
@@ -0,0 +1,10 @@
+// 2024/03/29 12:06:17 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "GUI/Controls/Painter.h"
+
+
+class PainterMelody : public Painter
+{
+public:
+    PainterMelody(wxWindow *, int width, int height);
+};
