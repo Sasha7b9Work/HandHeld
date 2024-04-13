01962f55e65a70dd79356fad5ab760174c6941c1
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-04-13 16:33:00 +0300

    *

--- sources/Device/src/Menu/Pages/PageWatch.cpp
+++ sources/Device/src/Menu/Pages/PageWatch.cpp
@@ -1,4 +1,4 @@
-// 2024/04/13 13:39:54 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+// 2024/04/13 13:35:35 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Menu/Pages/Pages.h"
 #include "Menu/MenuItemDef.h"
@@ -6,6 +6,21 @@
 
 namespace PageWatch
 {
+    namespace PageTime
+    {
+        DEF_TIME(timeTime, self, "ÂÐÅÌß", false);
+
+        DEF_PAGE_1(pageTime, PageMain::self, "ÂÐÅÌß",
+            &timeTime,
+            nullptr,
+            nullptr,
+            nullptr
+        );
+
+        const Item *const self = &pageTime;
+    }
+
+
     namespace PageDate
     {
         DEF_TIME(timeDate, PageDate::self, "ÄÀÒÀ", false);
