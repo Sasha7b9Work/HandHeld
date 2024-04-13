0c4dcaef7469c3cc8cf47e49b2abe0d86a51e510
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-04-13 23:29:41 +0300

    дополнительные настройки для будильника

--- sources/Device/src/Menu/Pages/PageAlarm.cpp
+++ sources/Device/src/Menu/Pages/PageAlarm.cpp
@@ -10,11 +10,41 @@ namespace PageAlarm
 
     DEF_DATE(date, self, "ƒ¿“¿", false);
 
+    DEF_CHOICE_10(choiceMelody, self, "Ã≈ÀŒƒ»ﬂ", (uint8 *const)&gset.alarm.melody,
+        Melody::Name(Melody::_1),
+        Melody::Name(Melody::_2),
+        Melody::Name(Melody::_3),
+        Melody::Name(Melody::_4),
+        Melody::Name(Melody::_5),
+        Melody::Name(Melody::_6),
+        Melody::Name(Melody::_7),
+        Melody::Name(Melody::_8),
+        Melody::Name(Melody::_9),
+        Melody::Name(Melody::_10)
+    );
 
+    DEF_CHOICE_3(choiceVolume, self, "√–ŒÃ Œ—“‹", (uint8 *const)&gset.alarm.volume,
+        "Õ»« ¿ﬂ",
+        "—–≈ƒÕﬂﬂ",
+        "¬€—Œ ¿ﬂ"
+    );
 
-    DEF_PAGE_2(pageAlarm, PageMain::self, "¡”ƒ»À‹Õ» ",
+    DEF_CHOICE_7(choiceColorBell, self, "÷¬≈“", (uint8 *const)&gset.alarm.colorBell,
+        ColorBell::Name(ColorBell::White),
+        ColorBell::Name(ColorBell::Red),
+        ColorBell::Name(ColorBell::Orange),
+        ColorBell::Name(ColorBell::Yellow),
+        ColorBell::Name(ColorBell::Green),
+        ColorBell::Name(ColorBell::Blue),
+        ColorBell::Name(ColorBell::Fiolet)
+    );
+
+    DEF_PAGE_5(pageAlarm, PageMain::self, "¡”ƒ»À‹Õ» ",
         &time,
         &date,
+        &choiceMelody,
+        &choiceVolume,
+        &choiceColorBell,
         nullptr,
         nullptr,
         nullptr
