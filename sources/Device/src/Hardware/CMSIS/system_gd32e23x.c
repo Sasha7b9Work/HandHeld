2252a5720f09f1e52dfb679bbaf7b8fe3df844e5
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-04-16 19:33:49 +0300

    *

--- sources/Device/src/Hardware/CMSIS/system_gd32e23x.c
+++ sources/Device/src/Hardware/CMSIS/system_gd32e23x.c
@@ -45,8 +45,8 @@
 
 /* select a system clock by uncommenting the following line */
 //#define __SYSTEM_CLOCK_8M_HXTAL              (__HXTAL)
-//#define __SYSTEM_CLOCK_8M_IRC8M              (__IRC8M)
-#define __SYSTEM_CLOCK_72M_PLL_HXTAL         (uint32_t)(78000000)
+#define __SYSTEM_CLOCK_8M_IRC8M              (__IRC8M)
+//#define __SYSTEM_CLOCK_72M_PLL_HXTAL         (uint32_t)(78000000)
 //#define __SYSTEM_CLOCK_72M_PLL_IRC8M_DIV2    (uint32_t)(8000000)
 
 #define RCU_MODIFY(__delay)     do{                                     \
