c3bde6887f1d24188d3493582676c7e694713efb
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2024-05-13 11:41:13 +0300

    Таблица преобразования русских символов в английские

--- sources/Device/src/Display/Display1602.cpp
+++ sources/Device/src/Display/Display1602.cpp
@@ -2,6 +2,17 @@
 #include "defines.h"
 #include "Display/Display1602.h"
 #include "Modules/WH1602B/WH1602B.h"
+#include <cstring>
+
+
+namespace Display
+{
+    static char buffer[2][16];
+
+    static void BeginScene();
+    static void EndScene();
+    static void Convert();
+}
 
 
 void Display::Init()
@@ -12,9 +23,9 @@ void Display::Init()
 
 void Display::Update()
 {
-    pchar buffer = "12345678ABCDEFGHABCDEFGH12345678";
+    BeginScene();
 
-    WH1602B::Write((uint8*)buffer);
+    EndScene();
 }
 
 
@@ -45,4 +56,45 @@ void Display::PrepareToSleep()
 uint Display::TimeEnabled()
 {
     return 0;
+}
+
+
+void Display::BeginScene()
+{
+    std::memset(buffer, ' ', 32);
+}
+
+
+void Display::EndScene()
+{
+    Convert();
+
+    WH1602B::Write((uint8 *)buffer);
+}
+
+
+void Display::Convert()
+{
+    static const pchar symbols =
+        "                "
+        "                "
+        "                "
+        "                "
+        "ABBGDEJZIIKLMNOP"
+        "PCTYFXC4JJQYIIII"
+        "                "
+        "                ";
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            uint8 symbol = (uint8)buffer[i][j];
+
+            if (symbol > 127)
+            {
+                buffer[i][j] = symbols[symbol - 128];
+            }
+        }
+    }
 }
